97929f60138f0482c3df7d7f8cad90eaf007450b
Mark Tsai <mx0910@gmail.com>
2020-02-14 12:34:26 +0800

    Updates

---
+++ precache-manifest.237a6c6f2bf1da2226409d3c4537f0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25e046a8e71809526a86e02d30d101d",
+    "url": "/dailydrinks/index.html"
+  },
+  {
+    "revision": "0d4cb2ecbb4ba9bf9303",
+    "url": "/dailydrinks/static/css/main.5dfc7c57.chunk.css"
+  },
+  {
+    "revision": "4d3ceb83aef652ef8631",
+    "url": "/dailydrinks/static/js/2.1b034ea1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dailydrinks/static/js/2.1b034ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4cb2ecbb4ba9bf9303",
+    "url": "/dailydrinks/static/js/main.d116bbd7.chunk.js"
+  },
+  {
+    "revision": "6f8d7c9e3fab3478e2bc",
+    "url": "/dailydrinks/static/js/runtime-main.528d6e72.js"
+  }
+]);
